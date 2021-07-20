6e863afda465aace054a14e6e79b04382d0c249b
Anatoly Bugakov <anatoly.bugakov@outlook.com>
2021-07-20 20:18:43 +0100

    Fixing Import Error

--- places/serializers.py
+++ places/serializers.py
@@ -2,20 +2,16 @@ from .models import Place
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from django.contrib.auth import authenticate
-# from .constants import ACCOUNT_NOT_FOUND
 from django.utils.six import text_type
 
 class PlaceSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Place
-        fields = 'id', 'email', 'username', 'first_name', 'last_name', 'bio',
+
 
 
 class CreatePlaceSerializer(serializers.Serializer):
-    email = serializers.EmailField()
-    username = serializers.CharField()
-    first_name = serializers.CharField()
-    last_name = serializers.CharField()
-    password = serializers.CharField()
-    bio = serializers.CharField()
+    #amount = serializers.IntegerField(required=True, min_value=0, null=True)
+    #name = serializers.CharField(required=True, max_length=128)
+    pass
