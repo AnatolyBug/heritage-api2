93442067b5538d6fe9c8329e8544ac0e38ff7ea8
Anatoly Bugakov <anatoly.bugakov@outlook.com>
2021-07-28 08:54:30 +0100

    merge conflict

--- heritage/settings.py
+++ heritage/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'heritage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
