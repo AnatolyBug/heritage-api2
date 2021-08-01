e5deab956b126c5f88cf8b365df4c0b02eceee76
jswebguru <jswebguru77@gmail.com>
2021-08-02 07:49:15 +0800

    Add file uploads to the places

--- utils/aws.py
+++ utils/aws.py
@@ -10,7 +10,7 @@ def generate_aws_url(key, bucket, content_type) -> str:
         aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
         aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
         config=Config('s3v4'),
-        region_name=os.getenv('AWS_DEFAULT_REGION')
+        region_name=os.getenv('AWS_S3_REGION_NAME')
     )
     return s3.generate_presigned_url(
         ClientMethod="get_object",
