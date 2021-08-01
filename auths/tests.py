ac865b196bdbe4dcc30687a070c0c41e46aaa094
Anatoly Bugakov <anatoly.bugakov@outlook.com>
2021-08-01 22:10:22 +0100

    auth tests

--- auths/tests.py
+++ auths/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.test import TestCase
+from django.urls import reverse
+
+from auths.models import User
+
+class AuthorListViewTest(TestCase):
+
+    def bad_user_dict(self):
+        return dict(email='bad@user.com', username='', first_name='Test', last_name='User',
+        password='', bio='abc')
+
+    def user_dict(self):
+        return dict(email='test@user.com', username='testuser', first_name='Test', last_name='User',
+        password='Testuser123', bio='abc')
+
+    @classmethod
+    def setUpTestData(cls):
+        # Create 15 Users for pagination tests
+        number_of_users = 15
+
+        for user_id in range(number_of_users):
+            User.objects.create(
+                email='test@example.com',
+                username='Username' + str(user_id),
+                first_name=f'First Name {str(user_id)}',
+                last_name=f'Surname {str(user_id)}',
+            )
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/user/authors/')
+        self.assertEqual(response.status_code, 200)
