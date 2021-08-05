f82f87fdd60699356e8ce44a1b3d1c044e3bc1cd
jswebguru <jswebguru77@gmail.com>
2021-08-06 07:13:12 +0800

    Add guide map storage

--- users/tests.py
+++ users/tests.py
@@ -9,7 +9,7 @@ class UsersViewsTest(APITestCase):
 
     def setUp(self):
         user = dict(email='test@user.com', username='Testuser', first_name='Test',
-            last_name='User', password='Testuser123', bio='abc')
+                    last_name='User', password='Testuser123', bio='abc')
         rv = self.client.post('/api/auth/register/', data=user)
         rv = self.client.post('/api/auth/login/', data=dict(email=user['email'], password=user['password']))
 
@@ -22,10 +22,10 @@ class UsersViewsTest(APITestCase):
 
         for user_id in range(number_of_users):
             User.objects.create(email=f'test@example.com {str(user_id)}',
-                username=f'Username {str(user_id)}',
-                first_name=f'First Name {str(user_id)}',
-                last_name=f'Surname {str(user_id)}',
-                password='TestPassword123')
+                                username=f'Username {str(user_id)}',
+                                first_name=f'First Name {str(user_id)}',
+                                last_name=f'Surname {str(user_id)}',
+                                password='TestPassword123')
 
     def test_login(self):
         rv = self.client.get('/api/users/')
