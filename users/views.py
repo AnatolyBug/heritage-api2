cc55bc11c807788e3001c606bb5e2a12d64bb766
Anatoly Bugakov <anatoly.bugakov@outlook.com>
2021-09-04 11:39:28 +0100

    User Views Swagger

--- users/views.py
+++ users/views.py
@@ -1,23 +1,28 @@
-from django.db import IntegrityError
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, permissions
 from rest_framework.response import Response
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
 from auths.models import User
-from auths.serializers import UserSerializer, UserSerializer
+from auths.serializers import UserSerializer, PutUserSerializer
+from users.serializers import UserListSerializer
+from auths.views import UserInfoAPIView
 
 
 class UserViewSet(viewsets.ViewSet):
+    permission_classes = (permissions.IsAuthenticated,)
+    page_param = openapi.Parameter('page', openapi.IN_QUERY, description="page number", type=openapi.TYPE_INTEGER)
+    user_param = openapi.Parameter('user_id', openapi.IN_PATH, description="user id", type=openapi.TYPE_INTEGER)
 
-    @staticmethod
-    def list(request):
+    @swagger_auto_schema(manual_parameters=[page_param],
+                         responses={"200": UserListSerializer})
+    def list(self, request):
         user_id = request.user.id
         user_role = request.user.user_role
 
-        if user_role == 'superuser':
-            user_list = User.objects.exclude(id=user_id).order_by('-created_date')
-        else:
-            get_users = User.objects.exclude(id=user_id, is_active=False)
-            user_list = get_users.filter(user_role='customer').order_by('-created_date')
+        get_users = User.objects.exclude(id=user_id, is_active=False, is_staff=False)
+        user_list = get_users.filter(user_role='customer').order_by('-created_date')
 
         page = request.query_params.get('page', 1)
         paginator = Paginator(user_list, 20)
@@ -34,64 +39,15 @@ class UserViewSet(viewsets.ViewSet):
         total = paginator.num_pages
         data = UserSerializer(users, many=True).data
 
-        return Response({'data': data,
-                         'previous_page': previous_page,
-                         'next_page': next_page,
-                         'total': total
-                         }, status=status.HTTP_200_OK)
+        # Put response into a serializer
+        return Response({'data': data, 'previous_page': previous_page, 'next_page': next_page,
+                         'total': total}, status=status.HTTP_200_OK)
 
-    @staticmethod
-    def retrieve(request, pk=None):
-        user_role = request.user.user_role
-        user = User.objects.filter(pk=pk).first()
+    @swagger_auto_schema(responses={"200": UserSerializer, "404": "Not Found"})
+    def retrieve(self, request, pk):
+        user = User.objects.filter(pk=pk, is_active=True).first()
         if user:
-            if user_role == 'superuser' or user_role == 'admin':
-                serializer = UserSerializer(user)
-                return Response(data=serializer.data, status=status.HTTP_200_OK)
-            else:
-                if not user.is_active:
-                    return Response(data='Not Found', status=status.HTTP_404_NOT_FOUND)
-                else:
-                    serializer = UserSerializer(user)
-                    return Response(data=serializer.data, status=status.HTTP_200_OK)
+            serializer = UserSerializer(user)
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(data='Not Found', status=status.HTTP_404_NOT_FOUND)
-
-    @staticmethod
-    def update(request, pk=None):
-        user_role = request.user.user_role
-
-        if user_role == 'superuser' or user_role == 'admin':
-            user = User.objects.filter(pk=pk).first()
-            if not user:
-                return Response({'error': 'No such user exists'}, status=status.HTTP_404_NOT_FOUND)
-            try:
-                user.username = request.data['username'].lower()
-                user.first_name = request.data['first_name']
-                user.last_name = request.data['last_name']
-                user.email = request.data['email']
-                user.save()
-            except IntegrityError:
-                return Response({
-                    'message': 'Field already exists.',
-                    'errors': {'field': 'Already exists.'}
-                }, status=status.HTTP_400_BAD_REQUEST)
-
-            return Response(data=UserSerializer(user).data, status=status.HTTP_200_OK)
-        else:
-            response = 'You are not allowed to update this customer.'
-            return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
-
-    @staticmethod
-    def destroy(request, pk=None):
-        user_role = request.user.user_role
-        if user_role == 'superuser' or user_role == 'admin':
-            user = User.objects.filter(pk=pk).first()
-            if not user:
-                return Response({'error': 'No such user exists'}, status=status.HTTP_404_NOT_FOUND)
-            user.is_active = False
-            user.save()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        else:
-            response = 'You are not allowed to delete this customer.'
-            return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
